1ad65fc3acd36674c5fcb5d93488dab31d38a76c
Tiany <1320736796@qq.com>
2024-07-12 15:03:22 +0800

    1

---
+++ _03Structural/src/Main.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author YTYYYY
+ * @version 1.0
+ * @date 2024/7/6-16:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ _07OptionalAPI/src/Main.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author YTYYYY
+ * @version 1.0
+ * @date 2024/7/12-14:46
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
